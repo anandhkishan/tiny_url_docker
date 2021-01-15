08e87152b451f6d25f949a65dc9c047558b457df
anandhkishan <anandhrockstar@gmail.com>
2021-01-15 19:57:56 +0530

    no cache build

--- tiny_automate_docker.sh
+++ tiny_automate_docker.sh
@@ -1,3 +1,3 @@
-docker build -t tiny/fe ./FE
-docker build -t tiny/be ./BE
+docker build --no-cache -t --no-cache tiny/fe ./FE
+docker build --no-cache -t --no-cache tiny/be ./BE
 docker-compose up -d
